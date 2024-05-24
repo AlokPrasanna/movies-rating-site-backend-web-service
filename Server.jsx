c990d184ab2d2485737f31212f9a10cc69af5d5e
AlokPrasanna <alokprasanna1104@gmail.com>
2024-05-24 14:14:41 +0530

    Modified code

--- Server.jsx
+++ Server.jsx
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv/config");
 
-const Configs = require('./configurations')
+const Configs = require('./configurations');
 
 const app = express();
 app.use(cors());
@@ -16,51 +16,49 @@ const db = mysql.createConnection({
     host: Configs.HOST,
     user: Configs.USER,
     password: Configs.PASSWORD,
-    port:Configs.DB_PORT,
+    port: Configs.DB_PORT,
     database: Configs.DATABASE
 });
 
 db.connect((err) => {
     if (err) {
-      console.error('Error connecting to MySQL: ' + err.stack);
-      return;
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
     }
     console.log('Connected to MySQL as id ' + db.threadId);
-  });
+});
 
-app.post("/add-new-movie-id", (req,res) => {
+app.post("/add-new-movie-id", (req, res) => {
     const MovieId = req.body.Id;
-    console.log(req.body)
+    console.log(req.body);
     if (!MovieId) {
         return res.status(400).json({ error: "Missing movieId in the request body" });
     }
 
     const CheckIfExitMovieId = "SELECT * FROM movies_rating WHERE movie_id = ? ";
-    db.query(CheckIfExitMovieId,[MovieId], (err,results) => {
-        if(err){
+    db.query(CheckIfExitMovieId, [MovieId], (err, results) => {
+        if (err) {
             console.error(err);
-            return res.status(500).json({error: "Internal Server Error! 1"});
+            return res.status(500).json({ error: "Internal Server Error! 1" });
         }
 
-        if(results.length > 0){
-            return res.status(200).json({message: "Already Exists"});
-        }else{
-            const InsertQuery = "INSERT INTO movies_rating (movie_id) VALUES (?)";
-            db.query(InsertQuery,[MovieId],(err,results) => {
-             
-                if(err){
+        if (results.length > 0) {
+            return res.status(200).json({ message: "Already Exists" });
+        } else {
+            const InsertQuery = "INSERT INTO movies_rating (movie_id, rating_count) VALUES (?, ?)";
+            db.query(InsertQuery, [MovieId, 0], (err, results) => {
+                if (err) {
                     console.error(err);
-                    return res.status(500).json({error: "Internal Server Error! 2"});
-                } 
-                return res.status(201).json({message: "Movie Id save successfully!"})
+                    return res.status(500).json({ error: "Internal Server Error! 2" });
+                }
+                return res.status(201).json({ message: "Movie Id save successfully!" });
             });
         }
     });
-    
 });
 
-app.get("/", function(req, res){
-    res.send("Hi Im here");
+app.get("/", function (req, res) {
+    res.send("Hi I'm here");
 });
 
 app.get("/get-movie-ratings", function (req, res) {
@@ -74,41 +72,40 @@ app.get("/get-movie-ratings", function (req, res) {
     });
 });
 
-app.put("/update-rating",(req,res) =>{
+app.put("/update-rating", (req, res) => {
     const MovieId = req.body.movieId;
     const RatingCount = req.body.ratingValue;
     const FansCount = req.body.newFansCount;
 
-    console.log("Movie Id : ",MovieId);
-    console.log("Rating Count : ",RatingCount);
-    console.log("Fans Count : ",FansCount);
+    console.log("Movie Id: ", MovieId);
+    console.log("Rating Count: ", RatingCount);
+    console.log("Fans Count: ", FansCount);
 
-    if(MovieId === null || RatingCount === null || FansCount === null){
+    if (MovieId === null || RatingCount === null || FansCount === null) {
         return res.status(400).json({ error: "Missing movieId in the request body" });
-    }else{
+    } else {
         const CheckIfExitMovieId = "SELECT * FROM movies_rating WHERE movie_id = ? ";
-        db.query(CheckIfExitMovieId,[MovieId], (err,results) => {
-        if(err){
-            console.error(err);
-            return res.status(500).json({error: "Internal Server Error! 1"});
-        }
-        if(results.length > 0){
-            const SaveQuery = "UPDATE movies_rating SET rating_count = ? , fans_count = ? WHERE movie_id = ?";
-            db.query(SaveQuery,[RatingCount,FansCount,MovieId],(err,result) =>{
-            if(err){
+        db.query(CheckIfExitMovieId, [MovieId], (err, results) => {
+            if (err) {
                 console.error(err);
-                return res.status(500).json({error: "Internal Server Error!"});
-            } 
-            return res.status(201).json({message: "Rating Update successfully!"})
-          });
-        }
-        else{
-            return res.status(400).json({ error: "Id Not Found!" }); 
-        }
-    });
+                return res.status(500).json({ error: "Internal Server Error! 1" });
+            }
+            if (results.length > 0) {
+                const SaveQuery = "UPDATE movies_rating SET rating_count = ?, fans_count = ? WHERE movie_id = ?";
+                db.query(SaveQuery, [RatingCount, FansCount, MovieId], (err, result) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({ error: "Internal Server Error!" });
+                    }
+                    return res.status(201).json({ message: "Rating Update successfully!" });
+                });
+            } else {
+                return res.status(400).json({ error: "Id Not Found!" });
+            }
+        });
     }
 });
 
 app.listen(port, () => {
     console.log("Server running...");
-  });
+});
